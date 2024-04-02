5ab97450dac9e6c49558de752474db9734b39220
Nickfit1 <163335212+Nickfit1@users.noreply.github.com>
2024-04-02 20:26:59 +0300

    Add files via upload

---
+++ Лабораторные работы/Функции/lab6.py
@@ -0,0 +1,15 @@
+# TODO Напишите функцию для поиска индекса товара
+def founded_items(list_, find_item):
+    if find_item in list_:
+            return list_.index(find_item)
+    else:
+        return None
+
+items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
+
+for find_item in ['банан', 'груша', 'персик']:
+    index_item = founded_items(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
+    if index_item is not None:
+        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
+    else:
+        print(f"Товар '{find_item}' не найден в списке.")
